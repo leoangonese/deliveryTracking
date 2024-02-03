7336e674b4ef7766740358c0c72e7d61473f92fe
Leonardo Angonese <leostaniecki@gmail.com>
2024-02-02 22:16:01 -0300

    center footer

--- frontend/src/components/layout/Footer.js
+++ frontend/src/components/layout/Footer.js
@@ -1,6 +1,6 @@
 const Footer = () => {
     return (
-        <footer className="w-full p-5 text-sm md:text-base  bg-white font-light text-black">
+        <footer className="w-full p-5 text-sm md:text-base  bg-white font-light text-black text-center">
             <a target="_blank" href="https://leoangonese.github.io/portfolio/">Desenvolvido por Leonardo Staniecki Angonese</a>
         </footer>
     )
